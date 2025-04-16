33d0687f8ebb262d75aa3116e364536e19c54618
SR0204 <k023g0023@g.neec.ac.jp>
2025-04-16 11:12:23 +0900

    プロジェクト ファイルを追加します。

---
+++ DirectXGame/main.cpp
@@ -0,0 +1,40 @@
+#include "KamataEngine.h"
+#include <Windows.h>
+
+using namespace KamataEngine;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	// 初期化処理
+
+
+	// エンジンの初期化
+	KamataEngine::Initialize(L"LE3C_13_スズキレオ");
+
+
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// 更新処理
+	while (true) {
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		// 描画開始
+		dxCommon->PreDraw();
+
+		// ここに描画処理を書く
+
+		dxCommon->PostDraw();
+	}
+
+	// 解放処理
+
+	// エンジンの終了処理
+	KamataEngine::Finalize();
+
+	return 0;
+}
