4f4e7cf7b765323700868a2007bb84cc3bca0241
SR0204 <k023g0023@g.neec.ac.jp>
2025-06-18 12:27:42 +0900

    完了

--- DirectXGame/Resources/shaders/TestPS.hlsl
+++ DirectXGame/Resources/shaders/TestPS.hlsl
@@ -1,11 +1,16 @@
+#include"Test.hlsli"
+
 struct PixcelShaderOutput
 {
     float32_t4 color : SV_TARGET0;
 };
 
-PixcelShaderOutput main()
+PixcelShaderOutput main(VertexShaderOutput input)
 {
     PixcelShaderOutput output;
-    output.color = float32_t4(1.0f, 1.0f, 1.0f, 1.0f);
+    
+    float32_t2 uv = input.texcoord;
+    
+    output.color = float32_t4(uv.x, uv.y, 0.0f, 1.0f);
     return output;
 }
